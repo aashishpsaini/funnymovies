0251b43c74e8d0f7278f816a244d2abadc747fea
Aashish Saini <asaini65@gmail.com>
2020-08-08 22:46:09 +0530

    changed method implementation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password]) || (user = new_user.save)
+    if user && user.authenticate(params[:password]) || (user = new_user)
       session[:user_id] = user.id
       redirect_to root_url, notice: 'Logged in!'
     else
@@ -24,6 +24,6 @@ class SessionsController < ApplicationController
   end
 
   def new_user
-    User.new(email: params[:email], password_digest: password_digest)
+    User.create(email: params[:email], password_digest: password_digest)
   end
 end
